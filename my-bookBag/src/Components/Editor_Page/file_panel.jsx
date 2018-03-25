f7d49af65d55210cda8bd711e32cd1859720b8ae
Michael-vdL <mvonderlippe@ansfederal.com>
2018-03-25 13:43:42 -0400

    Added resource folder to read from for notebooks in bookbags.

---
+++ my-bookBag/src/Components/Editor_Page/file_panel.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import NotebookButton from "./NotebookButton.jsx";
+
+class FilePanel extends Component {
+  render() {
+    var notebooks = require("./../../BookbagData/science_bag.json"); //Takes in a bookbag json. We have to make it so it takes in the one related to what page we are on
+
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h5>Bookbag Name Goes Here</h5>
+            </div>
+          </div>
+        </div>
+        <ul>
+          {notebooks.map(function(obj, index) {
+            return <NotebookButton notebook={obj} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+export default FilePanel;
