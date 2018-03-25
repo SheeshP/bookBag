07d9d604efcf711c82f6729f45582051f6a38c76
Karl <Karl@github.com>
2018-03-25 16:47:43 -0400

    Calendar

---
+++ my-bookBag/src/Components/Editor_Page/SideLeft.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './SideLeft.css';
+import Cal from './Cal.jsx';
+
+class SideLeft extends Component {
+
+
+  render () {
+    return (
+
+
+      <Menu right>
+
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+
+    );
+  }
+}export default SideLeft;
