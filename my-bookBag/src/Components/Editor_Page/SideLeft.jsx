613593e35bb0e899a6afdc2b5090511c0a6b8a28
Karl <Karl@github.com>
2018-03-29 13:41:52 -0400

    Blocks

--- my-bookBag/src/Components/Editor_Page/SideLeft.jsx
+++ my-bookBag/src/Components/Editor_Page/SideLeft.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import MiddleScreen from './MiddleScreen.jsx';
 import './SideLeft.css';
 import Cal from './Cal.jsx';
 
@@ -62,41 +62,11 @@ class SideLeft extends Component {
 
               <Cal/>
           </nav>
-
-
-          <div id="content">
-                  <div class="container-fluid">
-                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                          <ul class="nav navbar-nav navbar-right">
-                              <li><a href="#">Page</a></li>
-                              <li><a href="#">Page</a></li>
-                              <li><a href="#">Page</a></li>
-                              <li><a href="#">Page</a></li>
-                          </ul>
-                      </div>
-                  </div>
-
-
-              <h2>Collapsible Sidebar Using Bootstrap 3</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-              <div class="line"></div>
-
-              <h2>Lorem Ipsum Dolor</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-              <div class="line"></div>
-
-              <h2>Lorem Ipsum Dolor</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-              <div class="line"></div>
-              <h3>Lorem Ipsum Dolor</h3>
-
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div>
+          <MiddleScreen/>
           </div>
-      </div>
+          </div>
+
 
 
     );
